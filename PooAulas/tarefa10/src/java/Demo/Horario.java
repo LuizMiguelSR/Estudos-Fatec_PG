a9cc630a3ab77d81da067e7e97fcdf4b66cd2ff7
Luiz Miguel <70536705+LuizMiguelSR@users.noreply.github.com>
2022-04-25 16:46:29 -0300

    tarefa 10

--- PooAulas/tarefa10/src/java/Demo/Horario.java
+++ PooAulas/tarefa10/src/java/Demo/Horario.java
@@ -3,16 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Demo2;
-
-import Demo.Data;
+package Demo;
 
 /**
  *
  * @author Fatec
  */
-public class Main {
-    public static void main(String[] args) {
-        Data hoje = new Data();       
-    }
+public class Horario {
+    private int
 }
